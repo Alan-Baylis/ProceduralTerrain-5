f7dff3b9d9514c930d3237b0ad761920163ffb5f
Sunave <antti.suniala@gmail.com>
2015-08-30 20:29:44 +0300

    Add NaiveTerrain, generate vertices

--- Assets/NaiveTerrain.cs
+++ Assets/NaiveTerrain.cs
@@ -1,13 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
 public class NaiveTerrain : MonoBehaviour {
 
+	public int size = 4;
+	public float cellSize = 1.0f;
+
 	void Start () {
-		
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		mesh.vertices = GenerateVertices();
+		mesh.triangles = GenerateTriangles();
+		mesh.uv = GenerateUVs();
+		mesh.RecalculateNormals();
+
 	}
-	
-	void Update () {
-		
+
+	Vector3[] GenerateVertices () {
+		int sideVertices = size + 1;
+		Vector3[] vertices = new Vector3[sideVertices * sideVertices];
+
+		for (int gx = 0; gx < sideVertices; gx++) {
+			for (int gz = 0; gz < sideVertices; gz++) {
+				vertices[gx * sideVertices + gz] = new Vector3(gx * cellSize, 0.0f, gz * cellSize);
+			}
+		}
+
+		return vertices;
+	}
+
+	int[] GenerateTriangles () {
+		return null;
+	}
+
+	Vector2[] GenerateUVs () {
+		return null;
 	}
 }
---
+++ Assets/ShowMeshVertices.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(MeshFilter))]
+public class ShowMeshVertices : MonoBehaviour {
+
+	void OnDrawGizmos () {
+		MeshFilter mf = GetComponent<MeshFilter>();
+		Mesh mesh = mf.sharedMesh;
+		Vector3[] vertices = mesh.vertices;
+
+		Gizmos.matrix = Matrix4x4.TRS (
+			transform.position,
+			transform.rotation,
+			transform.lossyScale
+		);
+
+		for (int i = 0; i < vertices.Length; i++) {
+			Gizmos.DrawSphere(vertices[i], 0.05f);
+		}
+
+
+		// Show normals
+		Vector3[] normals = mesh.normals;
+		Vector3 vertex = new Vector3();
+		Vector3 normal = new Vector3();
+
+		for (int i = 0; i < normals.Length; i++) {
+			normal = normals[i];
+			vertex = mesh.vertices[i];
+			normal = normal + vertex;
+
+			Gizmos.DrawLine (vertex, normal);
+		}
+	}
+}
