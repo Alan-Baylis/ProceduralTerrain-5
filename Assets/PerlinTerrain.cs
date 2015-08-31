481155ad1e882e773eef235b6642dc93d97f6528
Sunave <antti.suniala@gmail.com>
2015-08-31 15:10:06 +0300

    Add PerlinTerrain

---
+++ Assets/PerlinTerrain.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
+public class PerlinTerrain : MonoBehaviour {
+	
+	public int size = 4;
+	public float cellSize = 1.0f;
+	
+	void Start () {
+		Regenerate ();
+		
+	}
+	
+	public void Regenerate ()
+	{
+		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		mesh.vertices = GenerateVertices ();
+		mesh.triangles = GenerateTriangles ();
+		mesh.uv = GenerateUVs ();
+		mesh.RecalculateNormals ();
+	}
+	
+	Vector3[] GenerateVertices () {
+		Perlin noise = new Perlin();
+
+		int sideVertices = size + 1;
+		Vector3[] vertices = new Vector3[sideVertices * sideVertices];
+		
+		for (int gx = 0; gx < sideVertices; gx++) {
+			for (int gz = 0; gz < sideVertices; gz++) {
+				float x = gx * cellSize;
+				float z = gz * cellSize;
+				float height = noise.Get(x, z);
+				vertices[gx * sideVertices + gz] = new Vector3(gx * cellSize, height, gz * cellSize);
+			}
+		}
+		
+		return vertices;
+	}
+	
+	int[] GenerateTriangles () {
+		int verticesPerTriangle = 3;
+		int trianglesPerCell = 2;
+		int numberOfCells = size * size;
+		int[] triangles = new int[numberOfCells * trianglesPerCell * verticesPerTriangle];
+		int triangleIndex = 0;
+		for (int cx = 0; cx < size; cx++) {
+			for (int cz = 0; cz < size; cz++) {
+				int n = cx * (size + 1) + cz;
+				
+				// First triangle of the pair
+				triangles[triangleIndex] = n;
+				triangles[triangleIndex + 1] = n + 1;
+				triangles[triangleIndex + 2] = n + size + 2;
+				triangleIndex += 3;
+				
+				// Second triangle of the pair
+				triangles[triangleIndex] = n;
+				triangles[triangleIndex + 1] = n + size + 2;
+				triangles[triangleIndex + 2] = n + size + 1;
+				triangleIndex += 3;
+				
+			}
+		}
+		
+		return triangles;
+	}
+	
+	Vector2[] GenerateUVs () {
+		int sideVertices = size + 1;
+		Vector2[] uvs = new Vector2[sideVertices * sideVertices];
+		
+		for (int ux = 0; ux < sideVertices; ux++) {
+			for (int uz = 0; uz < sideVertices; uz++) {
+				uvs[ux * sideVertices + uz] = new Vector2((float)ux / size, (float)uz / size);
+			}
+		}
+		
+		return uvs;
+	}
+}
---
+++ Assets/PerlinTerrainEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(PerlinTerrain))]
+public class PerlinTerrainEditor : Editor {
+	
+	public override void OnInspectorGUI () 
+	{
+		DrawDefaultInspector ();
+		
+		PerlinTerrain terrain = (PerlinTerrain)target;
+		if (GUILayout.Button("Regenerate")) {
+			terrain.Regenerate();
+		}
+	}
+	
+}
